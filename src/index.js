6841defbdbfe3291f43e58066c07e035e325c613
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-22 09:37:32 +0300

    added clientside files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import "./styles.scss";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
