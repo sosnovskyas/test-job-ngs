c99c9a5f25f61c2ac29796d3be9590de9c97b150
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-22 19:00:37 +0300

    added component - not ащгтв

---
+++ src/components/notFound/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import React from "react";
+
+export default class NotFound extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (<div>404 - Not found</div>)
+  }
+}
