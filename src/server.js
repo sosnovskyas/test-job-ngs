6aaf0757b0755285e24efb148b812c7296268604
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-22 09:37:38 +0300

    added server

---
+++ src/server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const path = require('path');
+
+const webpackCnfig = require('./../webpack.config');
+
+const PORT = 3000;
+
+const server = express();
+const compiler = webpack(webpackCnfig);
+
+server.use(webpackDevMiddleware(compiler, {
+  publicPath: webpackCnfig.output.publicPath,
+  hot: true,
+  stats: {
+    colors: true,
+    hash: false,
+    version: false,
+    chunks: false,
+    children: false
+  }
+}));
+
+server.use(webpackHotMiddleware(compiler));
+
+server.set('view engine', 'jade');
+server.set('views', __dirname);
+
+server.use('/api/data', (req, res) => (
+  res.sendFile(path.join(__dirname, 'data.json'))
+));
+
+server.use('/', (req, res) => (
+  res.render('index', { title: 'Hey', message: 'Hello there!'})
+));
+
+server.listen(PORT, 'localhost', err => {
+  if (err) console.log(`=> ERROR:  ${err}`);
+  console.log(`=> Webpack server is running on port ${PORT}`);
+});
