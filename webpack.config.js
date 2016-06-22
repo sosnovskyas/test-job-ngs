2f8e5b35213092805c6edbac7cc3f9aeaf2b56db
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-22 09:36:42 +0300

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const pluginsProd = [
+  new ExtractTextPlugin('style.css', {allChunks: true}),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  })];
+const pluginsDev = [
+  new webpack.HotModuleReplacementPlugin(),
+  new ExtractTextPlugin('style.css', {allChunks: true})
+];
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+module.exports = {
+  watch: isDevelopment,
+  devtool: isDevelopment ? 'cheap-module-inline-cource-map' : null,
+
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/'
+  },
+
+  plugins: isDevelopment ? pluginsDev : pluginsProd,
+
+  module: {
+    loaders: [
+      {test: /\.js?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel']},
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract(['css', 'sass?sourceMap'])},
+      {test: /\.json$/, exclude: /node_modules/, loader: 'json-loader'},
+      {test: /\.jade/, exclude: /node_modules/, loader: 'jade-loader'}
+    ]
+  },
+  postcss: [autoprefixer]
+};
