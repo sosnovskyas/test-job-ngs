3260ddce833074911366dd3aa5b743c2d191743b
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-22 18:47:43 +0300

    added component - delete channel

---
+++ src/components/channelDelete/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react'
+
+export default class ChannelDelete extends React.Component {
+    constructor() {
+        super();
+
+    }
+
+    render() {
+        return (<div>
+
+        </div>);
+    }
+}
