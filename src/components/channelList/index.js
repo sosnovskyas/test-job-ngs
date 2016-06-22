e38d9e38f9da9550ecf47121115f73e7150000d4
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-22 19:17:49 +0300

    cocmetic changes

--- src/components/channelList/index.js
+++ src/components/channelList/index.js
@@ -2,7 +2,7 @@
 import "./channelList.scss";
 import React from "react";
 import ChannelListItem from "../channelListItem";
-import { Link } from 'react-router'
+import {Link} from "react-router";
 
 export default class ChannelList extends React.Component {
   constructor() {
@@ -22,9 +22,9 @@ export default class ChannelList extends React.Component {
     return <div className="channel-list">
       <Link to="/add">add</Link>
       {/*<button className="channel-add" onClick={this.onAdd}>
-        <div className="channel-add__plus">+</div>
-        Добавить новый канал
-      </button>*/}
+       <div className="channel-add__plus">+</div>
+       Добавить новый канал
+       </button>*/}
       <ul className="channel-list__items">
         {this.props.channels.map(item => this.makeItem(item))}
       </ul>
