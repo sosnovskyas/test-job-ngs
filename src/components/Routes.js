f8b949ba78557804382589c77f06557489eba4ac
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-22 19:01:04 +0300

    changed routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,10 +5,14 @@ import { Route, IndexRoute } from 'react-router'
 import App from './App'
 import ChannelShow from './channelShow'
 import ChannelAdd from './channelAdd'
+import ChannelEdit from './channelEdit'
+import NotFound from './notFound'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={ChannelShow}/>
     <Route path="/add" component={ChannelAdd} />
+    <Route path="/edit/:id" component={ChannelEdit} />
+    <Route path="*" component={NotFound}/>
   </Route>
 );
