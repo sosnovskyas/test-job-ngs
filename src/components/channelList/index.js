e6b7256a6a069fa7951332b9e5240fe626b81bbc
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-23 18:35:17 +0300

    remove comments

--- src/components/channelList/index.js
+++ src/components/channelList/index.js
@@ -21,10 +21,6 @@ export default class ChannelList extends React.Component {
     return <div className="channel-list">
       <Link to="/add" className="channel-list__add"><span className="channel-list__add-plus">+</span> Добавить новый
         канал</Link>
-      {/*<button className="channel-add" onClick={this.onAdd}>
-       <div className="channel-add__plus">+</div>
-       Добавить новый канал
-       </button>*/}
       <ul className="channel-list__items">
         {this.props.channels.map(item => this.makeItem(item, (this.props.current == item.id)))}
       </ul>
