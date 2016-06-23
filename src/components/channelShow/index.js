4779a52e15b604ba3c013a80db2a94f8bae05a44
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-23 21:59:27 +0300

    get data

--- src/components/channelShow/index.js
+++ src/components/channelShow/index.js
@@ -4,15 +4,38 @@ import "./channelShow.scss";
 import React from "react";
 
 export default class ChannelShow extends React.Component {
-  
+
   constructor(props) {
     super(props);
 
   }
 
+  componentWillMount() {
+    this._findCurrentChannel();
+  }
+
+
   render() {
     return (<div className="channel-show">
-      show {this.props.params.id}
+      state {this.state.id}
     </div>);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this._findCurrentChannel(nextProps.params.id);
+  }
+
+
+  _findCurrentChannel(id) {
+    let channels = JSON.parse(localStorage['channels']);
+    let res = channels.find(item => {
+      if (item.id == (id || this.props.params.id)) return item
+    });
+
+    this.setState({
+      id: res.id,
+      name: res.name
+
+    });
   }
 }
