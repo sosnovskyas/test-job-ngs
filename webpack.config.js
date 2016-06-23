b8d7a7acd832100d0e9e60f4eee0c4b7a152539a
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-23 19:26:25 +0300

    updated autoprefixer rules

--- webpack.config.js
+++ webpack.config.js
@@ -45,5 +45,5 @@ module.exports = {
       {test: /\.jade/, exclude: /node_modules/, loader: 'jade-loader'}
     ]
   },
-  postcss: [autoprefixer({ browsers: ['ie 10','opera 12'] })]
+  postcss: [autoprefixer({ browsers: ['ie >= 10','opera >= 12'] })]
 };
