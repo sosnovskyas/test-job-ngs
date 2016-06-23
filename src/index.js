c7e0801d7f6ecd7ca2c145cb2dbdde2c85cf26ac
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-23 18:28:15 +0300

    added load data first time

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 'use strict';
 
 import "./styles.scss";
-
+import serverApi from "./components/serverApi";
 import React from "react";
 import {render} from "react-dom";
-import {Router, browserHistory} from "react-router"
-import routes from './components/Routes'
+import {Router, browserHistory} from "react-router";
+import routes from "./components/Routes";
 
 
-render(
-  <Router routes={routes} history={browserHistory}/>,
-  document.getElementById('root')
-);
+serverApi.getChannels()
+  .then(result => {
+    let channels = '';
+
+    try {
+      channels = JSON.stringify(result);
+      localStorage['channels'] = channels;
+    } catch (err) {
+      new Error("Bad data from server: ", err);
+    }
+
+    render(
+      <Router routes={routes} history={browserHistory}/>,
+      document.getElementById('root')
+    );
+
+  })
+  .catch(err => {
+    // alert('Получение списка каналов с сервера произошло с ошибкой: ', err);
+    new Error("Bad data from server: ", err);
+  });
+
+
