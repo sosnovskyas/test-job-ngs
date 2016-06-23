4f4a53df0a197ad3894327c2ec4a2f3f9bec88ca
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-23 16:14:14 +0300

    changed channel list style

--- src/components/channelList/index.js
+++ src/components/channelList/index.js
@@ -18,9 +18,8 @@ export default class ChannelList extends React.Component {
   }
 
   render() {
-    //<ChannelAdd text="" onClick={this.onAdd}/>
     return <div className="channel-list">
-      <Link to="/add">add</Link>
+      <Link to="/add" className="channel-list__add"><span className="channel-list__add-plus">+</span> Добавить новый канал</Link>
       {/*<button className="channel-add" onClick={this.onAdd}>
        <div className="channel-add__plus">+</div>
        Добавить новый канал
--- src/components/channelListItem/index.js
+++ src/components/channelListItem/index.js
@@ -11,7 +11,7 @@ export default class ChannelListItem extends React.Component {
     let item = this.props.item;
     let active = this.props.active ? 'channel-list-item_active' : '';
     return <li className={"channel-list-item " + active}>
-      <Link to={"/show/"+item.id}>
+      <Link className="channel-list-item__link" to={"/show/"+item.id}>
         <div className="channel-list-item__left">
           <img src={item.img} alt=""/>
         </div>
