ba1ed6625eb9c36080ebde471891336ac50f3fba
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-23 20:13:25 +0300

    changed show component

--- src/components/channelShow/index.js
+++ src/components/channelShow/index.js
@@ -1,16 +1,18 @@
 'use strict';
 
+import "./channelShow.scss";
 import React from "react";
 
 export default class ChannelShow extends React.Component {
-  constructor() {
-    super();
+  
+  constructor(props) {
+    super(props);
 
   }
 
   render() {
-    return (<div>
-      show
+    return (<div className="channel-show">
+      show {this.props.params.id}
     </div>);
   }
 }
