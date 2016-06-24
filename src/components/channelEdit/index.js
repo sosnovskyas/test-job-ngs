401a2e60cd9f281251b0b73fac0f62338ac370fc
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-24 16:45:57 +0300

    edit done

--- src/components/channelEdit/index.js
+++ src/components/channelEdit/index.js
@@ -2,6 +2,9 @@
 
 import "./channelEdit.scss";
 import React from "react";
+import api from "./../api";
+import {browserHistory} from "react-router";
+
 
 export default class ChannelEdit extends React.Component {
   constructor(props) {
@@ -9,13 +12,26 @@ export default class ChannelEdit extends React.Component {
 
   }
 
+  componentWillMount() {
+    let channel = JSON.parse(localStorage['channels'])[this.props.params.id - 1];
+    console.log(channel);
+    this.setState(channel);
+  }
+
+  componentDidMount() {
+    document.forms.edit
+      .addEventListener('submit', event => this._onSubmit(event));
+  }
+
   render() {
+    let item = this.state;
+
     return <div className="channel-edit">
       <div className="channel-edit__header">
         Форма редактирования канала
       </div>
       <form
-        name="add"
+        name="edit"
         className="channel-edit__form">
         <label>
           Название
@@ -23,7 +39,9 @@ export default class ChannelEdit extends React.Component {
             type="text"
             name="name"
             placeholder="Название"
-            className="channel-edit__input"/>
+            className="channel-edit__input"
+            value={item.name}
+            onChange={ event =>this._onChange(event, 'name')}/>
         </label>
         <label>
           ссылка
@@ -31,20 +49,31 @@ export default class ChannelEdit extends React.Component {
             type="url"
             name="url"
             placeholder="URL"
-            className="channel-edit__input"/>
+            className="channel-edit__input"
+            value={item.url}
+            onChange={ event =>this._onChange(event, 'url')}/>
         </label>
         <label>
           описание
           <textarea
             name="description"
             placeholder="описание"
-            className="channel-edit__input"/>
+            className="channel-edit__input"
+            value={item.description}
+            onChange={ event =>this._onChange(event, 'description')}/>
         </label>
         <input
           type="submit"
-          value="добавить"/>
+          value="изменить"/>
       </form>
-    </div>;
+    </div>
+      ;
+  }
+
+  _onChange(event, value) {
+    this.setState({
+      [value]: event.target.value
+    });
   }
 
   _onSubmit(event) {
@@ -52,13 +81,12 @@ export default class ChannelEdit extends React.Component {
   }
 
   _editChannel(id, value) {
-
-    console.log(id,value);
-    /*let channels = JSON.parse(localStorage['channels']);
+    let channels = JSON.parse(localStorage['channels']);
     let result = [];
     let i = 0;
 
     channels.map(item => {
+      if (item.id == (this.props.params.id)) item = value;
       i++;
       item.id = i;
       result.push(item);
@@ -67,8 +95,8 @@ export default class ChannelEdit extends React.Component {
     api.setChannelList(result)
       .then(data => {
         localStorage['channels'] = JSON.stringify(data);
-        browserHistory.push(`/show/${newChannel.id}`)
+        browserHistory.push(`/show/${id}`)
       });
-*/
+
   }
 }
