1372a927b4aeee6eeb285aea1b75172f6038e94d
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-24 10:52:31 +0300

    update api

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const path = require('path');
+const fs = require('fs');
 
 const webpackCnfig = require('./../webpack.config');
 
@@ -31,14 +32,39 @@ server.use(webpackHotMiddleware(compiler));
 server.set('view engine', 'jade');
 server.set('views', __dirname);
 
-server.use('/api/data', (req, res) => (
-  res.sendFile(path.join(__dirname, 'data.json'))
-));
+server.use('/api/data', (req, res) => {
+  fs.readFile(path.join(__dirname, 'data.json'), (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  });
+});
+
+server.get('/api/channels', (req, res) => {
+  fs.readFile(path.join(__dirname, 'channels.json'), (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  });
+});
 
 server.use('/', (req, res) => (
   res.render('index', {title: 'Hey', message: 'Hello there!'})
 ));
 
+server.post('/api/data', function(req, res) {
+  // if(!req.body.hasOwnProperty('author') ||
+  //   !req.body.hasOwnProperty('text')) {
+  //   res.statusCode = 400;
+  //   return res.send('Error 400: Post syntax incorrect.');
+  // }
+  //
+  // var newQuote = {
+  //   author : req.body.author,
+  //   text : req.body.text
+  // };
+  //
+  // quotes.push(newQuote);
+  res.json(true);
+});
 server.listen(PORT, '192.168.2.221', err => {
   if (err) console.log(`=> ERROR:  ${err}`);
   console.log(`=> Webpack server is running on port ${PORT}`);
