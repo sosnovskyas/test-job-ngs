0c2b28260e8bc090e6892c202009b2565e2ac820
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-24 11:15:47 +0300

    cosmetic changes

--- src/server.js
+++ src/server.js
@@ -36,14 +36,6 @@ server.set('views', __dirname);
 server.use(bodyParser.json()); // for parsing application/json
 server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
-server.use('/api/data', (req, res) => {
-  fs.readFile(path.join(__dirname, 'data.json'), (err, data) => {
-    if (err) throw err;
-    res.json(JSON.parse(data))
-  });
-});
-
 server.get('/api/channels', (req, res) => {
   fs.readFile(path.join(__dirname, 'channels.json'), (err, data) => {
     if (err) throw err;
@@ -59,9 +51,8 @@ server.post('/api/channels', (req, res) => {
 });
 
 server.use('/', (req, res) => (
-  res.render('index', {title: 'Hey', message: 'Hello there!'})
+  res.render('index', {})
 ));
-
 
 server.listen(PORT, '192.168.2.221', err => {
   if (err) console.log(`=> ERROR:  ${err}`);
