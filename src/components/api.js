a9b873d2e552b95eeae2f179f7a754b919e0743a
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-24 12:28:33 +0300

    added put method

--- src/components/api.js
+++ src/components/api.js
@@ -31,6 +31,47 @@ class Api {
 
     })
   }
+
+  setChannelList(data) {
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      let channels = [];
+
+      xhr.open('POST', '/api/channels', true);
+      xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+      xhr.onload = function () {
+        if (this.status == 200) {
+          try {
+            channels = JSON.parse(xhr.responseText);
+          } catch (err) {
+            reject(new Error('Неверный формат данных на сервере'));
+          }
+          resolve(channels);
+
+        } else {
+          let error = new Error(this.statusText);
+          error.code = this.status;
+          reject(error);
+        }
+      };
+
+      xhr.onerror = () => {
+        reject(new Error("Network Error"));
+      };
+
+      data = JSON.stringify([{
+        "id": 14,
+        "name": "channel 4",
+        "img": "./ch4.jpg",
+        "description": "The channel number 4",
+        "url": "http://static.feed.rbc.ru/rbc/internal/rss.rbc.ru/rbc.ru/mainnews.rss"
+      }]);
+      console.log('send', data);
+      xhr.send(data);
+
+    })
+  }
 }
 
 export default new Api();
