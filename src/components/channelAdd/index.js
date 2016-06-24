19ca1ab284096df67df59a15e518fa31e6230074
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-06-24 13:07:33 +0300

    submit form

--- src/components/channelAdd/index.js
+++ src/components/channelAdd/index.js
@@ -1,15 +1,38 @@
 "use strict";
 
-import './channelAdd.scss'
-
+import "./channelAdd.scss";
 import React from "react";
 
 export default class ChannelAdd extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    document.forms.add
+      .addEventListener('submit', event => this._onSubmit(event))
   }
 
   render() {
-    return (<div className="channel-add">add</div>)
+    return (<div className="channel-add">
+      <div className="channel-add__header">
+        Форма добавления нового канала
+      </div>
+      <form name="add">
+        <input type="text" name="name" placeholder="Название" required/>
+        <input type="url" name="url" placeholder="URL" required/>
+        <input type="submit" value="добавить"/>
+      </form>
+    </div>)
+  }
+
+  _onSubmit(event) {
+    event.preventDefault();
+
+    let url = event.target.url.value;
+    let name = event.target.name.value;
+
+    console.log(name, url);
+
   }
 }
